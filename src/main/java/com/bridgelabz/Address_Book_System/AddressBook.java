623d64e62603a31ccfd2f1fa30518dd3574c896d
sonaligadge24 <sonaligadge24@gmail.com>
2021-09-11 22:51:22 +0530

    Added: Display message

---
+++ src/main/java/com/bridgelabz/Address_Book_System/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Address_Book_System;
+
+
+public class AddressBook 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Welcome to address book program");
+    }
+}
